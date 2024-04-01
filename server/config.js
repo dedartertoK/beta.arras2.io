d5d1eb41c73b59d9c2d48c92a620298366c2efce
LA3T <142343914+LORDARRAS3000TESTER@users.noreply.github.com>
2024-04-01 11:37:37 +0200

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If 'localhost:NUMBER', the port must equal the port setting.
-    host: "beta-arras2-io.vercel.app",
+    host: "p2ht2z-3000.csb.app",
 
     // Which port to run the web server on.
     port: 3000,
