09442e9246178b46b7c4df99a85eb0bcdbd308c5
Kot <kotthekman@gmail.com>
2024-11-16 18:51:00 +0000

    permissions.js

--- server/permissions.js
+++ server/permissions.js
@@ -1,11 +1,11 @@
 module.exports = [
     {
         "key": "TOKEN_AImjndmOEPJOQ6d5756168fgee5f1sd_TOKEN",
-        "discordID": "0",
-        "nameColor": "#FDF380",
+        "discordID": "1203049886833049600",
+        "nameColor": "#FF0000",
         "class": "developer",
-        "name": "unnamed#0000",
-        "note": "note here"
+        "name": "thekotman",
+        "note": "The Co-Manager, Head Administrator and Co-Owner of arras2.io(confirmed from LA3T)."
     },
     {
         "key": "TOKEN_Aafdkjoiuowihbgoijfaklsmd;;x;AFIOHO",
