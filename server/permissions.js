0e8e0ae57b4ad45801bf93567bd63f449fc53fe7
Kot <kotthekman@gmail.com>
2024-11-16 17:35:48 +0000

    permissions.js

--- server/permissions.js
+++ server/permissions.js
@@ -1,10 +1,10 @@
 module.exports = [
     {
         "key": "TOKEN_AImjndmOEPJOQ6d5756168fgee5f1sd_TOKEN",
-        "discordID": "0",
-        "nameColor": "#FDF380",
+        "discordID": "1203049886833049600",
+        "nameColor": "#FF0000",
         "class": "developer",
-        "name": "unnamed#0000",
+        "name": "Kot",
         "note": "note here"
     },
     {
